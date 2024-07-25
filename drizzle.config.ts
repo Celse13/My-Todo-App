61b8fdc53f1cfe385dd11ed9d300b28cfdddaaa1
celse13 <rugiracelse13@gmail.com>
2024-07-25 15:17:13 +0200

    fixing drizzle deployment

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,13 @@
-import dotenv from 'dotenv';
-import { defineConfig  } from 'drizzle-kit';
+import dotenv from "dotenv";
+import { defineConfig } from "drizzle-kit";
 
 dotenv.config({ path: ".env.local" });
 
-// Define the configuration for Drizzle
 export default defineConfig({
-    schema: './database/schema.ts',
-    out: './src/generated',
+    schema: "./src/database/schema.ts",
+    out: "./src/generated",
     dialect: "postgresql",
     dbCredentials: {
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL!,
     },
 });
-
