3beabd66a52f2f569ef69472e8e0587e44c09975
celse13 <rugiracelse13@gmail.com>
2024-08-22 09:58:52 +0200

    Adding profile image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { FaUserCircle } from "react-icons/fa";
 import Link from "next/link";
 import { googleSignOut } from "@/actions/authActions";
+import { useSession } from "next-auth/react";
+import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const { data: session } = useSession();
 
   const handleSignOut = () => {
     googleSignOut();
@@ -12,35 +14,44 @@ const Navbar = () => {
   };
 
   return (
-      <div className="flex items-center sticky top-0 z-40 md:rounded-b-lg border justify-between 2xl:w-xl xl:w-lg md:w-[90%] w-full mx-auto h-16 md:px-8 px-5 md:py-3 bg-light">
-        <div>
-          <Link href={"/todos"}>
-            <h3 className="md:text-3xl text-2xl">
-              Todo-
-              <span className="text-[#0352fc] font-[700] drop-shadow">
+    <div className="flex items-center sticky top-0 z-40 md:rounded-b-lg border justify-between 2xl:w-xl xl:w-lg md:w-[90%] w-full mx-auto h-16 md:px-8 px-5 md:py-3 bg-light">
+      <div>
+        <Link href={"/todos"}>
+          <h3 className="md:text-3xl text-2xl">
+            Todo-
+            <span className="text-[#0352fc] font-[700] drop-shadow">
               app
             </span>
-            </h3>
-          </Link>
-        </div>
-
-        <div className="ml-auto relative">
-          <FaUserCircle
-              className="text-4xl mb-2 cursor-pointer"
-              onClick={() => setShowDropdown(!showDropdown)}
-          />
-          {showDropdown && (
-              <div className="absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg">
-                <button
-                    className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
-                    onClick={handleSignOut}
-                >
-                  Sign Out
-                </button>
-              </div>
-          )}
-        </div>
+          </h3>
+        </Link>
       </div>
+
+      <div className="ml-auto relative">
+        {session?.user?.image ? (
+          <img
+            src={session.user.image}
+            alt="User Profile"
+            className="w-10 h-10 rounded-full cursor-pointer"
+            onClick={() => setShowDropdown(!showDropdown)}
+          />
+        ) : (
+          <FaUserCircle
+            className="text-4xl mb-2 cursor-pointer"
+            onClick={() => setShowDropdown(!showDropdown)}
+          />
+        )}
+        {showDropdown && (
+          <div className="absolute right-0 mt-2 w-48 bg-white border rounded shadow-lg">
+            <button
+              className="block w-full px-4 py-2 text-left text-gray-700 hover:bg-gray-100"
+              onClick={handleSignOut}
+            >
+              Sign Out
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
